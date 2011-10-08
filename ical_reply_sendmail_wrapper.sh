2563a654f4cdc9bc837a761e6403cccddb90bb1f
Christian G. Warden <cwarden@xerus.org>
2011-10-07 22:21:00 -0700

    change content-type

--- ical_reply_sendmail_wrapper.sh
+++ ical_reply_sendmail_wrapper.sh
@@ -6,6 +6,7 @@ SENDMAIL=`mutt -D|awk -F= '/^sendmail=/{print $2}' | sed -s 's/"//g'`
 # fix header for ical reply and pipe to sendmail
 sed '/^Content-Type: text\/calendar;/s/$/; METHOD="REPLY"/'|
 sed '/^Content-Disposition: attachment/s/attachment/inline/g'|
+sed '/^Content-Type: multipart\/mixed/s/mixed/alternative/g'|
 $SENDMAIL "$@"
 
 # this may also be necessary:
